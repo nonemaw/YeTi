3773af771353c14849e6d7dcf9f9f29b5985a14e
nonemaw <michaelxuchj@gmail.com>
2017-11-29 21:05:36 +1100

    minor changes

--- common/meta.py
+++ common/meta.py
@@ -3,7 +3,7 @@ from common.crypto import AESCipher
 
 company = 'ytml'
 company_username = 'ytmladmin'
-company_password = 'Passw0rdOCT'
+company_password = ''
 """
 `company_xxx` variable will be modified when user logged in, and reset
 when logged out, is accessible to all other modules across the application
