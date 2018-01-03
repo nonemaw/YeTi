8dd67202305025f68d2a277146b6bb20e36918a9
nonemaw <michaelxuchj@gmail.com>
2018-01-03 18:02:07 +1100

    still testing post requests for interface

--- fuzzier/interface_test.py
+++ fuzzier/interface_test.py
@@ -17,92 +17,121 @@ interface_header = {
     'referer': 'https://ytml.xplan.iress.com.au/factfind/edit_interface',
 }
 
-menu_req = {"method": "ajax.MenuTreeAjax_rpc_load_node_gG7QNYAS_",
-            "params": [{"interface_type": "client",
-                        "menu_path": ""
-                        }],
-            "id": 1}
+menu_req = {
+    "method": "ajax.MenuTreeAjax_rpc_load_node_gG7QNYAS_",
+    "params": [{
+        "interface_type": "client",
+        "menu_path": ""
+    }],
+    "id": 1
+}
 
-# TODO: assumen my id is `custom_page_207_0_5`, under father `client_52-2-0`
+"""
+FMD SoA Wizard - [client_52]
+    FMD SMSF SoA Wizard - [client_52-3]
+        Assets & Liabilities - [client_52-3-1]
+                -> `52-3-1` as `menu_path`
+            Add Assets and Liabilities - [custom_page_208_1_12]
+                -> `custom_page_208` as `custom_page_name`
+                -> `12` as `field_index`
+                -> `1` as `subpage_index`
+"""
 leaf_req = {
     "method":"ajax.PageElementSettingAjax_rpc_html_kYjvPyv3_",
-    "params":[{
-        "custom_page_name":"incexp",  # TODO
-        "interface_type":"client",    # TODO
-        "menu_path":"",
-        "is_new":0,
-        "field_index":0,  # TODO
-        "subpage_index":0,
-        "requirement":-1,
-        "wrap_title":"",
-        "node_type":"page_element",
-        "publishable":False,
-        "is_wizard_subpage":0,
-        "can_import_export":False,
-        "children":[],
-        "is_scenario_wizard":0,
-        "has_conditions":None,
-        "title":"",
-        "tooltip":"",
-        "entity_type_condition_active":0,
-        "locales":[],
-        "removable":1,
-        "is_wizard":0,
-        "show_top_nav":0,
-        "hidden":False,
-        "immutable":0,
-        "editable":1,
-        "has_conditions_partner_page":0,
-        "node_id":"",
-        "url_target":"",
-        "post":"",
-        "partner_page_status":"none",
-        "entity_type_condition":[],
-        "alternative_title":"",
-        "url":"",
-        "hidden_xlite":False,
-        "entity_type":"client",
-        "element_type":"field",
-        "fieldname":"",
-        "mode":"edit",
-        "show_default":0,
-        "entity_types":[],
-        "display":"regular",
-        "tree":{},
-        "allow_cut":True,
-        "allow_paste":True,
-        "current_filters":{}
-    }],
+    "params":[
+        {
+            "custom_page_name": "",  # TODO
+            "interface_type": "client",
+            "menu_path": "",         # TODO
+            "field_index": 0,
+            "subpage_index": 0,      # TODO
+            "element_type": "",
+        }
+    ],
     "id":1
 }
 
-def handle_leaf(node_id: str, session: requests.sessions.Session, jison: Jison) -> str:
-    pass
+# TODO: depends on response of `leaf_req` - `title`: `Xplan`
+table1_method = "ajax.XplanElementListSettingAjax_rpc_html_WEaBDM8__"
+table2_method = "ajax.XplanElementEditSettingAjax_rpc_html_HBm947gH_"
+leaf_req_xtable = {
+    "method": "",
+    "params": [
+        {
+            "has_partner": False,
+            "domain": "factfind",
+            "guid": "00000000-0000-0000-0000-000000000000",
+            "cover_type": "",
+            "coa_access": False,
+            "entity_type": 0,
+            "locale": "AU",
+            "custom_page_name": "",
+            "editable": False,
+            "subpage_index": 0,
+            "partnerid": 0,
+            "mode": "edit",
+            "extra_params": "",
+            "field_index": 7,
+            "is_partner": False,
+            "entityid": 0,
+            "list_name": "",
+            "element_name": "",      # TODO, from response of `leaf_req` - `element_name`
+            "render_method": "factfind"
+        }
+    ],
+    "id": 1
+}
+
+def handle_leaf(node_id: str, menu_path: str, session: requests.sessions.Session, jison: Jison) -> str:
+    custom_page_name, index = re.search('(.+)_([\d]+_[\d]+)', node_id).groups()
+    subpage_index, field_index = [int(i) for i in index.split('_')]
+
+    leaf_req.get('params')[0]['menu_path'] = menu_path
+    leaf_req.get('params')[0]['custom_page_name'] = custom_page_name
+    leaf_req.get('params')[0]['subpage_index'] = subpage_index
+    leaf_req.get('params')[0]['field_index'] = field_index
+
+    pprint(leaf_req)
+
+
+    jison.load_json(session.post(TEST_URL, json=leaf_req, headers=interface_header).json())
+
+    print(jison.parse())
+
 
 
 def get_children(menu_path: str, session: requests.sessions.Session, jison: Jison) -> list:
+    """
+    get `children` under current `menu_path`
+
+    then acquire `sub_children` for each child in `children`
+    """
     menu_req.get('params')[0]['menu_path'] = menu_path
     jison.load_json(session.post(TEST_URL, json=menu_req, headers=interface_header).json())
 
+    # children list for current `menu_path` acquired
     hidden_status = jison.get_multi_object('hidden')
     children_id = jison.get_multi_object('node_id')
     children_title = jison.get_multi_object('title')
-
     children = []
-    father = children_id[-1]
 
+    # loop in `children`, acquire `sub_children` for each child
     for index, child_id in enumerate(children_id[:-1]):
         if hidden_status[index].get('hidden'):
             continue
 
-        sub_children = []
         child_path = re.search('client_([0-9_\-]+)', child_id.get('node_id'))
+        # normal node case (id starts with `client_xxx`)
         if child_path:
             child_path = child_path.group(1).replace('-', '/')
             sub_children = get_children(child_path, session, jison)
+        # leaf case (id not starts with `client_xxx`)
+        else:
+            sub_children = []
 
+        # if current child has no children (a leaf)
         if not sub_children:
-            leaf_type = handle_leaf(child_id.get('node_id'), session, jison)
+            leaf_type = handle_leaf(child_id.get('node_id'), menu_path, session, jison)
             if leaf_type:
                 children.append({
                     'id': child_id.get('node_id'),
@@ -115,6 +144,8 @@ def get_children(menu_path: str, session: requests.sessions.Session, jison: Jiso
                     'text': children_title[index].get('title'),
                     'type': 'other'
                 })
+
+        # else, append `sub_children` to current child
         else:
             children.append({
                 'id': child_id.get('node_id'),
@@ -136,10 +167,9 @@ def test(password: str):
     URL_LOGOUT = 'https://ytml.xplan.iress.com.au/home/logoff?'
 
 
-
     with requests.session() as session:
         payload = {
-            "userid": 'ytml1',
+            "userid": 'ytml2',
             "passwd": password,
             "rolename": "User",
             "redirecturl": ''
@@ -181,9 +211,11 @@ def test(password: str):
         #         node['children'] = get_children(menu_path, session, jison)
         #
         # pprint(menu)
+        #
+        #
+        # print(json.dumps(session.post(TEST_URL, json=menu_req, headers=interface_header).json()))
 
-
-        print(session.post(TEST_URL, json=leaf_req, headers=interface_header).json())
+        handle_leaf('custom_page_208_2_8', '52/3/2', session, jison)
 
 
 
