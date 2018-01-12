3929d838b0f94eb2a0292e92c79c1c386b638a65
nonemaw <michaelxuchj@gmail.com>
2018-01-12 17:39:28 +1100

    removed webdrivers

--- yeti.py
+++ yeti.py
@@ -20,11 +20,7 @@ if __name__ == '__main__':
                         help='Host address (default: 127.0.0.1)')
     parser.add_argument('--debug', '-D', required=False, action='store_true',
                         help='Debug Mode')
-    parser.add_argument('--fetcher', '-F', required=False, action='store_true',
-                        help='Run Fetcher')
-    parser.add_argument('--ifetcher', '-IF', required=False, action='store_true',
-                        help='Run Interface Fetcher')
-    parser.add_argument('--search', '-S', required=False, action='store_true',
+    parser.add_argument('--test', '-T', required=False, action='store_true',
                         help='Test')
     args = parser.parse_args()
 
@@ -34,7 +30,7 @@ if __name__ == '__main__':
         yeti.run(debug=True)
 
     # feature test
-    elif args.fetcher:
+    elif args.test:
         company, username, password = input(
             'Please input company, username and password: ').split(' ')
         groups = input('Please input groups (optional): ')
@@ -55,42 +51,11 @@ if __name__ == '__main__':
         Meta.menu_fetcher = MenuFetcher()
         Meta.menu_fetcher.fetch(groups)
 
-    # feature test
-    elif args.ifetcher:
-        company, username, password = input(
-            'Please input company, username and password: ').split(',')
-        specific = input('Please input specific nodes (optional): ')
-        if not specific:
-            specific = []
-        else:
-            specific = [x.strip() for x in specific.split(',') if x]
-
-        from common.interface_fetcher import InterfaceFetcher
-        from common.meta import Meta
-        from fuzzier.jison import Jison
-        from app.db import mongo_connect, client
-
-        Meta.company = company
-        Meta.company_username = username
-        Meta.company_password = password
-        Meta.db_company = Meta.db_default if Meta.company == 'ytml' else mongo_connect(
-            client, Meta.company)
-        Meta.jison = Jison(file_name=Meta.company)
-        Meta.interface_fetcher = InterfaceFetcher()
-        Meta.interface_fetcher.fetch(specific=specific)
-
-    # feature test
-    elif args.search:
-        from fuzzier.fuzzier import search
-        pattern = input('Input search pattern for test: ')
-        search(pattern=pattern)
-
     else:
         http_server = WSGIServer((args.host, args.port), yeti, log=None,
                                  handler_class=WebSocketHandler)
         try:
-            print(
-                "YeTi is running on http://{}:{}".format(args.host, args.port))
+            print(f'YeTi is running on http://{args.host}:{args.port}')
             http_server.serve_forever()
         except KeyboardInterrupt:
             pass
